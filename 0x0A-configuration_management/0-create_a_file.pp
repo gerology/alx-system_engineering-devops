97ddb20976c4f65826df9f5fda5157baba57b8b6
gerology <gerology007@gmail.com>
2023-03-30 19:50:43 +0000

    create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in /tmp
 file { 'school':
   ensure  => 'present',
-  path    => '/tmp/school',
+  content => 'I Love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I Love Puppet',
+  path    => '/tmp/school',
 }
