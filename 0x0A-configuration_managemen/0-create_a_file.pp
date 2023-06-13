3e3a584460ec06b6705c49fb0b838f7ede3065e9
gerology <gerology007@gmail.com>
2023-06-13 11:16:02 +0000

    removed

--- 0x0A-configuration_managemen/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# create a file in /tmp
-file { 'school':
-  ensure  => 'present',
-  path    => '/tmp/school',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I Love Puppet',
-  mode    => '0744',
-}
